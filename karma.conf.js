60a6a2c3bd01ffbb1d2539784d76a29074dec2e6
Eric W. Greene <eric@ericwgreene.com>
2016-06-17 13:52:13 -0700

    tweaked configuration

--- karma.conf.js
+++ karma.conf.js
@@ -9,18 +9,18 @@
 // process.env.SAUCE_ACCESS_KEY = '';
 
 var customLaunchers = {
-	'Local_Phantom': {
-		base: 'PhantomJS'
-	},
+	// 'Local_Phantom': {
+	// 	base: 'PhantomJS'
+	// },
 	// 'SL_Chrome': {
 	// 	base: 'SauceLabs',
 	// 	browserName: 'chrome'
 	// },
-	// 'SL_InternetExplorer': {
-	// 	base: 'SauceLabs',
-	// 	browserName: 'internet explorer',
-	// 	version: '10'
-	// },
+	'SL_InternetExplorer': {
+		base: 'SauceLabs',
+		browserName: 'internet explorer',
+		version: '10'
+	},
 	// 'SL_FireFox': {
 	// 	base: 'SauceLabs',
 	// 	browserName: 'firefox',
